4fd9f9777d5f99f011963010eb9ccb3f1f250765
Thomas Diggs <Thomas_Diggs@eogresources.com>
2023-12-07 10:26:58 -0700

    Day 3 Part One completed

--- Day 3/src/Main.java
+++ Day 3/src/Main.java
@@ -4,6 +4,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
 Code to show a list of files.
@@ -39,13 +41,166 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String filePath = "./Day 3/AdventofCode2023Day3/sample.txt";
+        String filePath = "./Day 3/input.txt";
 
         List<String> lines = reader(filePath);
 
-        for (String line : lines) {
-            System.out.println(line);
+//        System.out.println();
+//        for (String line : lines) {
+//            System.out.println(line);
+//        }
+
+        char[] symbols = {'*', '/', '@', '&', '+', '=', '#', '%', '-', '$'};
+
+        int sum = 0;
+
+        // For each line in lines
+        for (int i = 0; i < lines.size(); i++) {
+            Pattern pattern = Pattern.compile("\\d+");
+            Matcher matcher = pattern.matcher(lines.get(i));
+            // Find each number and its start and end indices
+            while (matcher.find()) {
+                String number = matcher.group();
+                int start = matcher.start();
+                int end = matcher.end();
+
+//                System.out.println("\nNumber: " + number);
+//                System.out.println("Start Index: " + start);
+//                System.out.println("End Index: " + end);
+//                System.out.println("Located on line: " + i + " of " + (lines.size() - 1));
+
+                char[] thisRow = lines.get(i).toCharArray();
+
+                boolean containsSymbol = false;
+
+                // CHECK CURRENT ROW
+                // Check left of number
+                if (start != 0) {
+                    for (char symbol : symbols) {
+                        if (thisRow[start - 1] == symbol) {
+                            containsSymbol = true;
+                        }
+                    }
+                }
+                // Check right of number
+                if (end != thisRow.length) {
+                    for (char symbol : symbols) {
+                        if (thisRow[end] == symbol) {
+                            containsSymbol = true;
+                        }
+                    }
+                }
+
+                // CHECK THE ROW ABOVE
+                // If row exists above current row
+                if (i != 0) {
+//                    System.out.println("Line above exists");
+                    char[] rowAbove = lines.get(i - 1).toCharArray();
+                    // Check if number is the middle
+                        // If start != 0 && end != lines.get(i - 1).length()
+                            // Check for symbol from start - 1 to end
+                    if (start != 0 && end != rowAbove.length) {
+                        int checkStart = start - 1;
+                        int checkEnd = end;
+                        for (int j = checkStart; j <= checkEnd; j++) {
+                            for (char symbol : symbols) {
+                                if (rowAbove[j] == symbol) {
+                                    containsSymbol = true;
+                                }
+                            }
+                        }
+                        // Check if number is at the left
+                            // Else if start == 0 and end != lines.get(i - 1).length()
+                                // Check for symbol from start to end
+                    } else if (start == 0 && end != rowAbove.length) {
+                        int checkStart = start;
+                        int checkEnd = end;
+                        for (int j = checkStart; j <= checkEnd; j++) {
+                            for (char symbol : symbols) {
+                                if (rowAbove[j] == symbol) {
+                                    containsSymbol = true;
+                                }
+                            }
+                        }
+                        // Check if number is at the right
+                            // Else if start != 0 and end == lines.get(i -1).length()
+                                // Check for symbol from start to end - 1
+                    } else if (start != 0 && end == rowAbove.length) {
+                        int checkStart = start - 1;
+                        int checkEnd = end - 1;
+                        for (int j = checkStart; j <= checkEnd; j++) {
+                            for (char symbol : symbols) {
+                                if (rowAbove[j] == symbol) {
+                                    containsSymbol = true;
+                                }
+                            }
+                        }
+                    }
+                } else {
+//                    System.out.println("Line above does not exist");
+                }
+
+                // CHECK THE ROW BELOW
+                // If row exists below current row
+                if (i != (lines.size() - 1)) {
+//                    System.out.println("Line below exists");
+                    char[] rowBelow = lines.get(i + 1).toCharArray();
+                    // Check if number is the middle
+                    // If start != 0 && end != lines.get(i - 1).length()
+                    // Check for symbol from start - 1 to end
+                    if (start != 0 && end != rowBelow.length) {
+                        int checkStart = start - 1;
+                        int checkEnd = end;
+                        for (int j = checkStart; j <= checkEnd; j++) {
+                            for (char symbol : symbols) {
+                                if (rowBelow[j] == symbol) {
+                                    containsSymbol = true;
+                                }
+                            }
+                        }
+                        // Check if number is at the left
+                        // Else if start == 0 and end != lines.get(i - 1).length()
+                        // Check for symbol from start to end
+                    } else if (start == 0 && end != rowBelow.length) {
+                        int checkStart = start;
+                        int checkEnd = end;
+                        for (int j = checkStart; j <= checkEnd; j++) {
+                            for (char symbol : symbols) {
+                                if (rowBelow[j] == symbol) {
+                                    containsSymbol = true;
+                                }
+                            }
+                        }
+                        // Check if number is at the right
+                        // Else if start != 0 and end == lines.get(i -1).length()
+                        // Check for symbol from start to end - 1
+                    } else if (start != 0 && end == rowBelow.length) {
+                        int checkStart = start - 1;
+                        int checkEnd = end - 1;
+                        for (int j = checkStart; j <= checkEnd; j++) {
+                            for (char symbol : symbols) {
+                                if (rowBelow[j] == symbol) {
+                                    containsSymbol = true;
+                                }
+                            }
+                        }
+                    }
+                } else {
+//                    System.out.println("Line below does not exist");
+                }
+
+                // If no symbols are found in the checks above, add number to sum.
+                if (containsSymbol) {
+                    sum += Integer.parseInt(number);
+                }
+
+//                System.out.println("Contains symbol is " + containsSymbol);
+
+            }
         }
 
+        System.out.println("\nSum: " + sum);
+
     }
+
 }
