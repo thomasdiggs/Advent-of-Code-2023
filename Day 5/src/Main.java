ce1d80fb564bf3248799ef4ed29d8d1b2ea8d4b3
Thomas Diggs <thomasdiggs@gmail.com>
2023-12-15 11:11:22 -0700

    Day 5 Part 2 - Will return with a new algorithm that doesn't take days to run.

--- Day 5/src/Main.java
+++ Day 5/src/Main.java
@@ -3,47 +3,28 @@ import java.util.Arrays;
 
 public class Main {
 
-//    public static void findMapping(ArrayList<Seed> seeds, String[] input, String setMethod, String getMethodSource, String getMethodDestination) {
-//        for (String line : input) {
-//            long destination = Long.parseLong(line.split(" ")[0]);
-//            long source = Long.parseLong(line.split(" ")[1]);
-//            long range = Long.parseLong(line.split(" ")[2]);
-//            for (Seed seed : seeds) {
-//                if (seed.callGetMethod(getMethodSource) >= source && seed.callGetMethod(getMethodSource) < source + range) {
-//                    seed.callSetMethod(setMethod, destination + (seed.callGetMethod(getMethodSource) - source));
-//                }
-//            }
-//        }
-//        for (Seed seed : seeds) {
-//            if (seed.callGetMethod(getMethodDestination) == 0) {
-//                seed.callSetMethod(setMethod, seed.callGetMethod(getMethodSource));
-//            }
-//        }
-//    }
-
-    //// TESTING
     public static long findMapping(long inputSource, String[] input) {
-        long outputDestination = inputSource;
         for (String line : input) {
-            long destination = Long.parseLong(line.split(" ")[0]);
-            long source = Long.parseLong(line.split(" ")[1]);
-            long range = Long.parseLong(line.split(" ")[2]);
+            String[] parts = line.split(" ");
+            long destination = Long.parseLong(parts[0]);
+            long source = Long.parseLong(parts[1]);
+            long range = Long.parseLong(parts[2]);
             if (inputSource >= source && inputSource < source + range) {
-                outputDestination = destination + (inputSource - source);
+                return destination + (inputSource - source);
             }
         }
-        return outputDestination;
+        return inputSource;
     }
-    //// TESTING
 
     public static void main(String[] args) {
+
+        long msStartMain = System.currentTimeMillis();
+        long nsStartMain = System.nanoTime();
 
         String filePath = "./Day 5/sample.txt";
         ArrayList<String> lines = CSVUtility.reader(filePath);
 
-//        ArrayList<Seed> seeds = new ArrayList<>();
-//        ArrayList<String> seeds = new ArrayList<>();
-        ArrayList<String> seeds = new ArrayList<>(Arrays.asList(lines.getFirst().replaceAll("seeds: ", "").split(" ")));
+        ArrayList<String> seedsInput = new ArrayList<>(Arrays.asList(lines.getFirst().replaceAll("seeds: ", "").split(" ")));
         ArrayList<String> seedToSoilMapInput = new ArrayList<>();
         ArrayList<String> soilToFertilizerMapInput = new ArrayList<>();
         ArrayList<String> fertilizerToWaterMapInput = new ArrayList<>();
@@ -52,10 +33,6 @@ public class Main {
         ArrayList<String> temperatureToHumidityMapInput = new ArrayList<>();
         ArrayList<String> humidityToLocationMapInput = new ArrayList<>();
 
-//        String seedsInput = lines.getFirst().replaceAll("seeds: ", "");
-//        for (String seed : seedsInput.split(" ")) {
-//            seeds.add(new Seed(Long.parseLong(seed), 0));
-//        }
         for (int i = 2; i < lines.size(); i++) {
             if (lines.get(i).startsWith("seed-to-soil map:")) {
                 i++;
@@ -108,80 +85,70 @@ public class Main {
             }
         }
 
-        //// TESTING
-        long seedID = 0;
-        long minimum = Long.MAX_VALUE;
-        for (String seed : seeds) {
-            long soil = findMapping(Long.parseLong(seed), seedToSoilMapInput.toArray(new String[0]));
-            long fertilizer = findMapping(soil, soilToFertilizerMapInput.toArray(new String[0]));
-            long water = findMapping(fertilizer, fertilizerToWaterMapInput.toArray(new String[0]));
-            long light = findMapping(water, waterToLightMapInput.toArray(new String[0]));
-            long temperature = findMapping(light, lightToTemperatureMapInput.toArray(new String[0]));
-            long humidity = findMapping(temperature, temperatureToHumidityMapInput.toArray(new String[0]));
-            long location = findMapping(humidity, humidityToLocationMapInput.toArray(new String[0]));
-            if (location < minimum) {
-                minimum = location;
-                seedID = Long.parseLong(seed);
+        String[] seedToSoilMap = seedToSoilMapInput.toArray(new String[0]);
+        String[] soilToFertilizerMap = soilToFertilizerMapInput.toArray(new String[0]);
+        String[] fertilizerToWaterMap = fertilizerToWaterMapInput.toArray(new String[0]);
+        String[] waterToLightMap = waterToLightMapInput.toArray(new String[0]);
+        String[] lightToTemperatureMap = lightToTemperatureMapInput.toArray(new String[0]);
+        String[] temperatureToHumidityMap = temperatureToHumidityMapInput.toArray(new String[0]);
+        String[] humidityToLocationMap = humidityToLocationMapInput.toArray(new String[0]);
+
+
+        System.out.println("\n--Part One");
+        long msStartPartOne = System.currentTimeMillis();
+        long nsStartPartOne = System.nanoTime();
+        long seedIDPartOne = 0;
+        long minLocationPartOne = Long.MAX_VALUE;
+        for (String seed : seedsInput) {
+            long currentSeed = Long.parseLong(seed);
+            long soil = findMapping(currentSeed, seedToSoilMap);
+            long fertilizer = findMapping(soil, soilToFertilizerMap);
+            long water = findMapping(fertilizer, fertilizerToWaterMap);
+            long light = findMapping(water, waterToLightMap);
+            long temperature = findMapping(light, lightToTemperatureMap);
+            long humidity = findMapping(temperature, temperatureToHumidityMap);
+            long location = findMapping(humidity, humidityToLocationMap);
+            if (location < minLocationPartOne) {
+                minLocationPartOne = location;
+                seedIDPartOne = Long.parseLong(seed);
             }
-//            System.out.println("Seed: " + seed +
-//                    " Soil: " + soil +
-//                    " Fertilizer: " + fertilizer +
-//                    " Water: " + water +
-//                    " Light: " + light +
-//                    " Temperature: " + temperature +
-//                    " Humidity: " + humidity +
-//                    " Location: " + location);
         }
-        System.out.println("\nPart One");
-        System.out.println("Seed ID: " + seedID + " has the lowest location: " + minimum);
-        //// TESTING
+        long msEndPartOne = System.currentTimeMillis();
+        long nsEndPartOne = System.nanoTime();
+        System.out.println("Seed ID: " + seedIDPartOne + " has the lowest location: " + minLocationPartOne);
 
-//        findMapping(seeds, seedToSoilMapInput.toArray(new String[0]), "setSoil", "getSeed", "getSoil");
-//        findMapping(seeds, soilToFertilizerMapInput.toArray(new String[0]), "setFertilizer", "getSoil", "getFertilizer");
-//        findMapping(seeds, fertilizerToWaterMapInput.toArray(new String[0]), "setWater", "getFertilizer", "getWater");
-//        findMapping(seeds, waterToLightMapInput.toArray(new String[0]), "setLight", "getWater", "getLight");
-//        findMapping(seeds, lightToTemperatureMapInput.toArray(new String[0]), "setTemperature", "getLight", "getTemperature");
-//        findMapping(seeds, temperatureToHumidityMapInput.toArray(new String[0]), "setHumidity", "getTemperature", "getHumidity");
-//        findMapping(seeds, humidityToLocationMapInput.toArray(new String[0]), "setLocation", "getHumidity", "getLocation");
-//
-//        for (Seed seed : seeds) {
-//            System.out.println(seed);
-//        }
-//
-//        long seedID = 0;
-//        long minimum = Long.MAX_VALUE;
-//        for (Seed seed : seeds) {
-//            if (seed.getLocation() < minimum) {
-//                minimum = seed.getLocation();
-//                seedID = seed.getSeed();
-//            }
-//        }
-//
-//        System.out.println("Seed ID: " + seedID + " has the lowest location: " + minimum);
 
-        // TESTING
-        String[] seedsRanges = lines.getFirst().replaceAll("seeds: ", "").split(" ");
-        long seedIDWithLowestLocation = 0;
-        long lowestLocation = Long.MAX_VALUE;
-        for (int i = 0; i < seedsRanges.length; i += 2) {
-            for (long j = 0; j < Long.parseLong(seedsRanges[i + 1]); j++) {
-                long currentSeed = Long.parseLong(seedsRanges[i]) + j;
-                long soil = findMapping(currentSeed, seedToSoilMapInput.toArray(new String[0]));
-                long fertilizer = findMapping(soil, soilToFertilizerMapInput.toArray(new String[0]));
-                long water = findMapping(fertilizer, fertilizerToWaterMapInput.toArray(new String[0]));
-                long light = findMapping(water, waterToLightMapInput.toArray(new String[0]));
-                long temperature = findMapping(light, lightToTemperatureMapInput.toArray(new String[0]));
-                long humidity = findMapping(temperature, temperatureToHumidityMapInput.toArray(new String[0]));
-                long location = findMapping(humidity, humidityToLocationMapInput.toArray(new String[0]));
-                if (location < lowestLocation) {
-                    lowestLocation = location;
-                    seedIDWithLowestLocation = currentSeed;
+        System.out.println("\n--Part Two");
+        long msStartPartTwo = System.currentTimeMillis();
+        long nsStartPartTwo = System.nanoTime();
+        long seedIDPartTwo = 0;
+        long minLocationPartTwo = Long.MAX_VALUE;
+
+        for (int i = 0; i < seedsInput.size(); i += 2) {
+            for (long j = 0; j < Long.parseLong(seedsInput.get(i + 1)); j++) {
+                long currentSeed = Long.parseLong(seedsInput.get(i)) + j;
+                long soil = findMapping(currentSeed, seedToSoilMap);
+                long fertilizer = findMapping(soil, soilToFertilizerMap);
+                long water = findMapping(fertilizer, fertilizerToWaterMap);
+                long light = findMapping(water, waterToLightMap);
+                long temperature = findMapping(light, lightToTemperatureMap);
+                long humidity = findMapping(temperature, temperatureToHumidityMap);
+                long location = findMapping(humidity, humidityToLocationMap);
+                if (location < minLocationPartTwo) {
+                    minLocationPartTwo = location;
+                    seedIDPartTwo = currentSeed;
                 }
             }
         }
-        System.out.println("\nPart Two");
-        System.out.println("Seed ID: " + seedIDWithLowestLocation + " has the lowest location: " + lowestLocation);
-        // TESTING
+        long msEndPartTwo = System.currentTimeMillis();
+        long nsEndPartTwo = System.nanoTime();
+        System.out.println("Seed ID: " + seedIDPartTwo + " has the lowest location: " + minLocationPartTwo);
 
+
+        long msEndMain = System.currentTimeMillis();
+        long nsEndMain = System.nanoTime();
+        System.out.println("\n\nPart One execution time: " + (msEndPartOne - msStartPartOne) + "ms" + ", " + (nsEndPartOne - nsStartPartOne) + "ns");
+        System.out.println("Part Two execution time: " + (msEndPartTwo - msStartPartTwo) + "ms" + ", " + (nsEndPartTwo - nsStartPartTwo) + "ns");
+        System.out.println("Program execution time: " + (msEndMain - msStartMain) + "ms" + ", " + (nsEndMain - nsStartMain) + "ns");
     }
 }
